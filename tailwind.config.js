f93f798bf60291b7676dd89860101e22f548d03c
Sebastian <semega1206@gmail.com>
2024-09-07 17:22:28 -0500

    definimosFonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Speedee", "system-ui", "sans-serif"], // Define tu fuente principal
+        serif: ["Merriweather", "serif"], // Fuente alternativa
+      },
+    },
   },
   plugins: [],
-}
-
+};
