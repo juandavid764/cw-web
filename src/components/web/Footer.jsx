1b651560249b29bd8ff94dcfcd389dc127d6f186
juandavid764 <juandaviderazo2401@gmail.com>
2024-09-22 16:32:54 -0500

    reponsive-footer-aboutPage-v1

--- src/components/web/Footer.jsx
+++ src/components/web/Footer.jsx
@@ -7,14 +7,14 @@ import wpIcon from "../../assets/socialMediaIcons/wpicon.svg"
 const Footer = () => {
     return (
 
-        <footer className="bg-black grid grid-cols-3 p-10">
+        <footer className="bg-black grid grid-cols-1 lg:grid-cols-3 lg:p-10 p-5">
 
-            <div className="">
-                <img src={cwLogo} alt="Company Logo" />
+            <div className="size-40 lg:size-72">
+                <img src={cwLogo} alt="Company Logo" className="" />
             </div>
-            <div className="flex flex-col content-center gap-7 ">
-                <h1 className="text-white text-4xl font-bold text-center">Síguenos:</h1>
-                <div className="flex flex-row place-content-around">
+            <div className="flex flex-col lg:content-center gap-7 ">
+                <h1 className="text-white lg:text-4xl font-bold lg:text-center">Síguenos:</h1>
+                <div className="flex flex-row gap-2 lg:place-content-around">
                     <a href="https://www.instagram.com/cartoon_war_?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="" rel="noopener noreferrer">
                         <button className="">
                             <img src={igIcon} alt="Facebook Icon" className="w-12 h-12" />
@@ -33,23 +33,24 @@ const Footer = () => {
                         </button>
                     </a>
                 </div>
-                <h2 className="text-white text-2xl font-semibold text-center">Desarrolladores:</h2>
-                <div className='flex flex-row place-content-around'>
+                <h2 className="text-white lg:text-2xl font-semibold lg:text-center">Desarrolladores:</h2>
+                <div className='flex flex-col lg:place-content-around'>
                     <a href="https://github.com/juandavid764" className='animate-pulse'>
-                        <h2 className="text-white text-2xl  text-center">Juan Trujillo</h2>
+                        <h2 className="text-white lg:text-2xl  lg:text-center">Juan David Trujillo</h2>
                     </a>
                     <a href="https://github.com/SebastianM1206" className='animate-pulse'>
-                        <h2 className="text-white text-2xl  text-center">Sebastian Medina</h2>
+                        <h2 className="text-white lg:text-2xl  lg:text-center">Sebastian Medina Garcia</h2>
+
                     </a>
                 </div>
             </div>
-            <div className="flex flex-col place-content-around">
-                <h2 className="text-white text-4xl font-bold text-center">Horarios:</h2>
-                <h2 className="text-white text-2xl font-semibold text-center">Todos los días</h2>
-                <h2 className="text-white text-2xl font-semibold text-center">6:00pm - 12am</h2>
-                <h3>Columna 3</h3>
-                <p>Contenido de la columna 3</p>
+            <div className="row-start-2 lg:row-start-1 lg:col-start-3 lg:align-top">
+                <h2 className="text-white lg:text-4xl font-bold lg:text-center">Horarios:</h2>
+                <br></br>
+                <h2 className="text-white lg:text-2xl  lg:text-center">Todos los días</h2>
+                <h2 className="text-white lg:text-2xl  lg:text-center">6:00pm - 12am</h2>
             </div>
+            <br></br>
 
         </footer >
     );
