d590ec057af50ef4f64e0981cf56c4029b7d600d
juandavid764 <juandaviderazo2401@gmail.com>
2024-12-07 17:47:43 -0500

    buttonSave

--- src/pages/pages_admin/AdminRequest.jsx
+++ src/pages/pages_admin/AdminRequest.jsx
@@ -14,13 +14,14 @@ const AdminRequest = () => {
     { id: 4, label: "Cancelado" },
   ];
 
-  const [isEditing, setIsEditing] = useState(false);
-  const [selectedBtn, setSelectedBtn] = useState(0);
+  const [selectedBtn, setSelectedStateBtn] = useState(0);
   const [selectedPedido, setSelectedPedido] = useState(null);
   const [formattedRequest, setFormattedRequest] = useState([]);
   const [searchTerm, setSearchTerm] = useState(""); // Estado para el texto de búsqueda
   const [newState, setNewState] = useState("Pendiente");
   const [newTotal, setNewTotal] = useState(0);
+
+  const [refreshData, setRefreshData] = useState(false);
 
   const clientDataRef = useRef();
   const productDataRef = useRef();
@@ -30,9 +31,17 @@ const AdminRequest = () => {
     async function fetchData() {
       const data = await getFormatRequest();
       setFormattedRequest(data || []);
+      console.log("Actualice la data");
+
     }
     fetchData();
-  }, []);
+
+    // const timer = setTimeout(() => {
+    //   setRefreshData(!refreshData);
+    // }, 3000);
+
+    // return () => clearTimeout(timer);
+  }, [refreshData]);
 
   const filteredPedidos = useMemo(() => {
     return formattedRequest.filter((pedido) => {
@@ -40,7 +49,7 @@ const AdminRequest = () => {
         selectedBtn === 0 || pedido.status === buttons[selectedBtn].label;
       const matchesSearch =
         pedido.client.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        Number(pedido.request_id).toString().includes(searchTerm );
+        Number(pedido.request_id).toString().includes(searchTerm);
       return matchesStatus && matchesSearch;
     });
   }, [formattedRequest, selectedBtn, searchTerm]);
@@ -91,21 +100,6 @@ const AdminRequest = () => {
 
   return (
     <div className="bg-gray-100 py-4">
-      {/* Encabezado */}
-      <div className="flex items-center justify-center mb-6 bg-orange-200 py-5 ml-[20vw] mr-[20vh] rounded-full">
-        <button
-          onClick={() => setIsEditing(!isEditing)}
-          className={`flex items-center px-5 py-2 rounded-md text-white font-semibold text-xl transition-colors ${
-            isEditing ? "bg-orange-500" : "bg-gray-700 hover:bg-gray-600"
-          }`}
-        >
-          <FontAwesomeIcon
-            icon={isEditing ? faTimes : faEdit}
-            className="mr-2"
-          />
-          {isEditing ? "Cancelar edición" : "Editar"}
-        </button>
-      </div>
 
       <div className="flex items-center  justify-around  gap-0 px-52 md:px-5">
         <section className="col-span-2" id="left">
@@ -119,16 +113,21 @@ const AdminRequest = () => {
         </section>
 
         <section className="col-span-5 flex flex-col items-center gap-10">
+          
           <div className="flex space-x-2">
+          <div>
+            <button className="bg-gray-900 text-white px-6 rounded-md" onClick={() => { setRefreshData(!refreshData)}}>
+              Refrescar
+            </button>
+          </div>
             {buttons.map((button) => (
               <button
                 key={button.id}
-                onClick={() => setSelectedBtn(button.id)}
-                className={`px-4 py-2 rounded-md font-medium transition-colors ${
-                  selectedBtn === button.id
+                onClick={() => setSelectedStateBtn(button.id)}
+                className={`px-4 py-2 rounded-md font-medium transition-colors ${selectedBtn === button.id
                     ? "bg-orange-400 text-white"
                     : "bg-gray-200 text-gray-700 hover:bg-gray-300"
-                }`}
+                  }`}
               >
                 {button.label}
               </button>
@@ -139,11 +138,10 @@ const AdminRequest = () => {
             <div className="border-2 border-orange-400 rounded-lg p-4 shadow-lg h-96 w-80">
               <h3 className="text-lg font-medium mb-2">Datos del cliente</h3>
               <textarea
-                readOnly
                 ref={clientDataRef}
-                className={`w-full h-56 p-2 rounded  resize-none border-4 border-gray-400 ${
-                  isEditing ? "" : "bg-gray-100 cursor-not-allowed"
-                }`}
+                className={`w-full h-56 p-2 rounded  resize-none border-4 border-gray-400
+                  
+                `}
               ></textarea>
               <div className="flex flex-col items-center justify-center py-2 gap-0">
                 <div className="flex items-center justify-center mb-5 gap-3">
@@ -153,16 +151,11 @@ const AdminRequest = () => {
                       <h1 className="p-0 m-0 ">Total:</h1>
                       <input
                         onChange={(e) => setNewTotal(e.target.value)}
-                        readOnly={!isEditing}
                         type="text"
                         ref={totalCostRef}
                         className={
-                          "border-2 border-black rounded-2xl w-36 pl-5 " +
-                          `${
-                            isEditing === false
-                              ? " hover:cursor-not-allowed bg-gray-500 text-gray-200"
-                              : "bg-white"
-                          }`
+                          "border-2 border-black rounded-2xl w-36 pl-5 bg-white"
+
                         }
                       />
                     </>
@@ -172,7 +165,7 @@ const AdminRequest = () => {
                 <DropdownStates
                   estado={newState}
                   setNewState={setNewState}
-                  disabled={!isEditing}
+
                 />
               </div>
             </div>
@@ -182,25 +175,19 @@ const AdminRequest = () => {
               <textarea
                 readOnly
                 ref={productDataRef}
-                className={`w-full h-64 p-2 rounded resize-none border-4 border-gray-400 ${
-                  isEditing ? "" : "bg-gray-100 cursor-not-allowed"
-                }`}
+                className={`w-full h-64 p-2 rounded resize-none border-4 border-gray-400`}
               ></textarea>
             </div>
           </div>
 
-          {isEditing ? (
-            <button
-              className="bg-orange-400 text-white px-6 rounded-md hover:bg-orange-300"
-              disabled={!isEditing}
-              onClick={handleSave}
-            >
-              <FontAwesomeIcon icon={faSave} className="mr-2" />
-              Guardar cambios
-            </button>
-          ) : (
-            ""
-          )}
+
+          <button
+            className="bg-orange-400 text-white px-6 rounded-md hover:bg-orange-300 py-4"
+            onClick={handleSave}
+          >
+            <FontAwesomeIcon icon={faSave} className="mr-2" />
+          </button>
+
         </section>
       </div>
     </div>
